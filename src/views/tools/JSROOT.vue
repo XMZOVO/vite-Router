aeabc85b995b743c0c91a23451ff518dcffeb32c
CarryIsabelle <2980999208@qq.com>
2022-03-20 16:42:11 +0800

    jsroot

--- src/views/tools/JSROOT.vue
+++ src/views/tools/JSROOT.vue
@@ -9,14 +9,14 @@ let geom_file = "https://jsroot.gsi.de/files/geom/evegeoshape.json.gz",
      async function draw_geometry_and_tracks() {
         let obj = await JSROOT.httpRequest(geom_file, "object");
         let geo_painter = await JSROOT.draw("drawing", obj);
-        // let file = await JSROOT.openFile(tracks_file);
-        // let tracks = await file.readObject("tracks;1");
+        let file = await JSROOT.openFile(tracks_file);
+        let tracks = await file.readObject("tracks;1");
  
         // one could simply draw list of tracks
         // JSROOT.draw("drawing", tracks, "same");
  
         // or use method of TGeoPainter to draw extras
-        // geo_painter.drawExtras(tracks, "tracks");
+        geo_painter.drawExtras(tracks, "tracks");
      }
  
      draw_geometry_and_tracks();
