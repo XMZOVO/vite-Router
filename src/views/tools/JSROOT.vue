305e128a508b2c053e06c7ca379146343fc96cbb
CarryIsabelle <2980999208@qq.com>
2022-03-21 09:35:36 +0800

    1234

--- src/views/tools/JSROOT.vue
+++ src/views/tools/JSROOT.vue
@@ -1,6 +1,6 @@
 <template>
     <iframe
-        src="../../../public/JsRoot631/index.htm?nobrowser&file=../../../public/JsRoot631/test.root&item=Default;1&opt=ssao;BACK;transp60"
+        src="/public/JsRoot631/index.htm?nobrowser&file=/public/JsRoot631/test.root&item=Default;1&opt=ssao;BACK;transp60"
         frameborder="0"
         class="w-full h-screen"
     ></iframe>
