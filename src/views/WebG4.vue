7db8b0bfb34ca5e435524d61951de3fb3adea7bd
CarryIsabelle <2980999208@qq.com>
2022-03-27 15:04:17 +0800

    fix-nuclide-vmodel

--- src/views/WebG4.vue
+++ src/views/WebG4.vue
@@ -25,6 +25,7 @@
                         class="form-label inline-block text-gray-700"
                     >核素选择</label>
                     <select
+                        v-model="nuclide"
                         class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                         aria-label="Default select example"
                     >
