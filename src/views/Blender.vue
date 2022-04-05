252e4d565814123ebc5eb829ae952ffc6ac87f5f
CarryIsabelle <2980999208@qq.com>
2022-04-05 09:56:21 +0800

    111

--- src/views/Blender.vue
+++ src/views/Blender.vue
@@ -137,7 +137,7 @@ onMounted(() => {
         antialias: true
     })
     renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    renderer.setPixelRatio(window.devicePixelRatio)
     renderer.physicallyCorrectLights = true
     renderer.outputEncoding = THREE.sRGBEncoding
     renderer.toneMapping = THREE.ReinhardToneMapping
